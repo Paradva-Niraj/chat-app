617303acd4252ef79cd891224405798c4e086f9f
niraj <nirajparadva2004@gmail.com>
2025-04-30 23:46:15 +0530

    nope!

--- src/components/ContactPanel.jsx
+++ src/components/ContactPanel.jsx
@@ -3,7 +3,7 @@ import '../Style/Input.css'
 function ContactPanel() {
     return ( 
         <>
-            <div className="h-screen-100 hidden md:block w-sm mt-4 ml-4 mb-4 rounded-xl background back border-2">
+            <div className="h-screen-100 block md:hidden w-sm mt-4 ml-4 mb-4 rounded-xl background back border-2">
                 contact
             </div>
         </>
