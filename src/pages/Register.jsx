728e53c4e6b88a4b184599b2abc6d6563632ef21
niraj <nirajparadva2004@gmail.com>
2025-05-07 21:38:16 +0530

    add start script

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,7 +15,7 @@ function Register() {
     const handleSubmit = async(e) =>{
         e.preventDefault();
         try{
-            const res = await axios.post('http://localhost:3000/api/auth/register',form);
+            const res = await axios.post(`${import.meta.env.VITE_AUTH_URL}register`,form);
             // alert(res.data.msg);
             nav('/login')
         }
