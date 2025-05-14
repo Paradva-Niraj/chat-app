5178246ef209bf25050e258ae30998449b0f1715
niraj <nirajparadva2004@gmail.com>
2025-05-14 15:14:32 +0530

    build changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"; // Make sure capitalization is correct
-import OtpVerification from "../components/otpVerification";
+import OtpVerification from "../components/OtpVerification";
     
 function Register() {
     const [form, setForm] = useState({ phone: '', password: '', name: '', dob: '' });
